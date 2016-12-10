f16a659c3a9dbfc8cd07714bb010fc2a562ed6d7
lanking520 <lanking520@gmail.com>
2016-12-09 23:56:09 -0500

    Add info

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
 /// </// <reference path="angular.min.js" />
 /// </// <reference path="highcharts.js" />
 var myApp = angular.module("Mainmodule", []);
-var preUrl = "http://localhost:8008";
+var preUrl = "http://35.162.120.77";
 var mainController = function ($scope, $http, $log) {
 	$scope.currView = "/home/home.html";
     $scope.init = function () {
        
+    }
+    $scope.homeinit = function(){
+    	$http({
+            url: preUrl + "/policy",
+            method: "GET"
+        }).success(function (response) {
+            var ldata  = response.pop();
+            $scope.networks = ldata["Networks"];
+            ldata.remove('Networks');
+            $scope.Phone = ldata;
+        });
     }
 
 }
