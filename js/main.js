9128a93f906e783b69c385c06ee93523eea1ec2a
lanking520 <lanking520@gmail.com>
2016-12-09 23:27:56 -0500

    Add main controller

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
+/// </// <reference path="angular.min.js" />
+/// </// <reference path="highcharts.js" />
+var myApp = angular.module("Mainmodule", []);
+var preUrl = "http://localhost:8008";
+var mainController = function ($scope, $http, $log, $window) {}
 
+myApp.controller("MainController", mainController);
