4da621d87f0838e53ed7a41c7fc632e143e3779d
lanking520 <lanking520@gmail.com>
2016-12-09 23:37:51 -0500

    Add home html

--- js/main.js
+++ js/main.js
@@ -2,6 +2,12 @@
 /// </// <reference path="highcharts.js" />
 var myApp = angular.module("Mainmodule", []);
 var preUrl = "http://localhost:8008";
-var mainController = function ($scope, $http, $log, $window) {}
+var mainController = function ($scope, $http, $log) {
+	$scope.currView = "./home.html";
+    $scope.init = function () {
+       
+    }
+    
+}
 
 myApp.controller("MainController", mainController);
