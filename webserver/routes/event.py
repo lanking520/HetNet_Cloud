98e58646c29f2c40c6069a6a9587a13004ede28f
Kai He <kh2789@columbia.edu>
2017-04-30 21:47:05 -0400

    add setapppref API

--- webserver/routes/event.py
+++ webserver/routes/event.py
@@ -5,8 +5,8 @@ import json
 from flask import request
 
 
-@routes.route('/event/gethighestbandbyuidloc', methods=['GET'])
-def get_highest_band_by_uid_loc():
+@routes.route('/event/getmacidbyprefbyuidloc', methods=['GET'])
+def get_macid_by_pref_by_uid_loc():
     """
     get appdata by uid
     :return: {
@@ -16,12 +16,12 @@ def get_highest_band_by_uid_loc():
     """
 
     uid_param = request.args.get('uid')
-    device_id_param = request.args.get('deviceid')
+    device_id_param = request.args.get('device_id')
     loc_param = request.args.get('location')
 
     try:
         cursor_select = g.conn.execute('SELECT preference FROM apppref WHERE uid = %s AND location = %s AND device_id = %s',
-                                       uid_param, loc_param, loc_param)
+                                       uid_param, loc_param, device_id_param)
 
         pref = ""
 
@@ -29,9 +29,9 @@ def get_highest_band_by_uid_loc():
             pref = row['perference']
 
         if pref == "highest bandwidth":
-            cursor_select = g.conn.execute('SELECT macid, MAX(bandwidth) FROM neteval')
+            cursor_select = g.conn.execute('SELECT N.macid FROM neteval N WHERE N.bandwidth = (SELECT MAX(bandwidth) FROM neteval) LIMIT 1')
         elif pref == "lowest latency":
-            cursor_select = g.conn.execute('SELECT macid, MIN(latency) FROM neteval')
+            cursor_select = g.conn.execute('SELECT N.macid FROM neteval N WHERE N.bandwidth = (SELECT MIN(latency) FROM neteval) LIMIT 1')
         else:
             cursor_select = g.conn.execute('SELECT macid FROM neteval WHERE macid = %s',
                                            pref)
@@ -50,3 +50,47 @@ def get_highest_band_by_uid_loc():
 
         response_json = {"Status": "Failure"}
         return Response(response=json.dumps(response_json), status=500, mimetype="application/json")
+
+
+
+@routes.route('/event/setapppref', methods=['POST'])
+def set_app_pref():
+    params = request.get_json()
+    uid = params['uid']
+    device_id = params['device_id']
+    loc = params['location']
+    time = params['time']
+    pref = params['pref']
+
+    try:
+        if pref != "highest bandwidth" and pref != "lowest latency":
+            cursor_select = g.conn.execute('SELECT macid FROM networkdata WHERE ssid = %s AND location = %s',
+                                           pref, loc)
+            for row in cursor_select:
+                pref = row['macid']
+
+        # Find if network already exists in database
+        cursor_select = g.conn.execute('SELECT * FROM apppref WHERE uid = %s AND location = %s AND device_id = %s',
+                                       uid, loc, device_id)
+        if cursor_select.rowcount == 0:
+            cursor_insert = g.conn.execute(
+                'INSERT INTO apppref(uid, device_id, location, preference, time) VALUES(%s, %s, %s, %s, %s)',
+                uid, device_id, loc, pref, time)
+        else:
+            for row in cursor_select:
+                row['preferece'] = pref
+                row['time'] = time
+
+        response_json = {"Status": "Success"}
+        return Response(response=json.dumps(response_json), status=200, mimetype="application/json")
+
+
+    except Exception as e:
+        print e
+
+        response_json = {"Status": "Failure"}
+        return Response(response=json.dumps(response_json), status=500, mimetype="application/json")
+
+
+# input uid, loc, ssid, pref
+# save in apppref (convert ssid & loc to macid)
